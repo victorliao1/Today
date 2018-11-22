4a42710a7b3d5d858d848ded4dc3bbf43ef66403
Victor Liao <victorliao18@gmail.com>
2018-11-22 01:33:13 -0500

    static file made and integrated js and css

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# definition for a list of directories where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
