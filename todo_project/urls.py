9bca7f3f052778048c675339925c33ee40ef3666
Victor Liao <victorliao18@gmail.com>
2018-11-24 11:18:02 -0500

    new path

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from todolist.views import todoView, addTodo, deleteTodo
 
 urlpatterns = [
+    path('/', todoView),
     path('admin/', admin.site.urls),
     path('todolist/', todoView),
     path('addTodo/', addTodo),
