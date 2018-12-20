c451b8b5ac6ca5b1216525a5a35ea3a995f465b2
Victor Liao <victorliao18@gmail.com>
2018-12-19 19:03:26 -0500

    dynamic background and style edits

--- static/todoapp/js/todo.js
+++ static/todoapp/js/todo.js
@@ -1,1 +1,9 @@
-console.log("this is working yay")
+var rand = Math.floor((Math.random()*20)+1);
+var imgCache = new Image();
+imgCache.src = '/static/todoapp/css/images/stock' + rand + '.jpg';
+$(window).on('load', function() {
+    $("body").css({ "background": "url(/static/todoapp/css/images/stock" + rand + ".jpg)",
+                   "background-size": "100% 100%",
+                   "background-repeat": "no-repeat"
+                  });
+});
